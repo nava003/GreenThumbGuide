d4112bbdc429c76264ca388e7e092b8211933cb7
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-07 20:43:16 -0500

    added files and file structure copied from mini project and edited a little bit

---
+++ Main/models/Plant.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Plant extends Model {}
+
+Plant.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    user_id: { // Keep? Maybe move
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Project;
