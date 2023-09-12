ce2d3d4ead4b6c257e84f331e6f4d1ea00a7de23
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-11 19:46:01 -0500

    added more code to api/index.js file

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -2,7 +2,18 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+// Middleware
+const errorMiddleware = require('./errorMiddleware');
+const authenticationMiddleware = require('./authenticationMiddleware');
+const validationMiddleware = require('./validationMiddleware');
+const loggingMiddleware = require('./loggingMiddleware');
+
+// Apply Middleware
+router.use(loggingMiddleware);
+router.use(errorMiddleware);
+
+// Routes
+router.use('/users', authenticationMiddleware, userRoutes);
+router.use('/projects', authenticationMiddleware, projectRoutes);
 
 module.exports = router;
