3b8892e8b8836385af29978fc9b567f0ea4e0449
NavaHub <navaa003@gmail.com>
2024-07-31 01:29:50 -0500

    Reworked connection.js to now execute TiDB's Database connection vs Heroku's JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,35 @@
 const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+// First IF Statement is an original connection to Heroku + JawsDB hosting service;
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// }
+
+// Second IF Statement is a new DB hosting service by TiDB to avoid reliance on Heroku;
+if (process.env.TIDB_URL) {
+  console.log('Entering TIDB...');
+  sequelize = new Sequelize(
+    process.env.TIDB_DATABASE,    
+    process.env.TIDB_USER,
+    process.env.TIDB_PASSWORD,
+    {
+      host: process.env.TIDB_HOST,
+      dialect: 'mysql',
+      port: process.env.TIDB_PORT,
+      dialectOptions: {
+        ssl: {
+          ca: fs.readFileSync(path.join(__dirname, 'isrgrootx1.pem'))
+        }
+      }
+    }
+  );
+} else { // Else, utilize a local MySQL connection for development purposes.
+  console.log('Others failed: Entering Localhost...');
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
